f8a112be07a4fc1b0f107e2163f3a520a9294436
frenky <frenky22@student.uisi.ac.id>
2025-07-19 17:34:54 +0700

    table keterangan

--- Back/php/get_chart_data.php
+++ Back/php/get_chart_data.php
@@ -1,31 +1,32 @@
 <?php
-include 'koneksi.php'; // pastikan koneksi berfungsi
+include 'koneksi.php';
 
-// Query yang benar
+$tahun = isset($_GET['tahun']) ? $_GET['tahun'] : '';
+$bulan = isset($_GET['bulan']) ? $_GET['bulan'] : '';
+
+$where = [];
+if ($tahun !== '') $where[] = "YEAR(ss.tanggal) = '$tahun'";
+if ($bulan !== '') $where[] = "MONTH(ss.tanggal) = '$bulan'";
+$whereClause = $where ? 'WHERE ' . implode(' AND ', $where) : '';
+
 $query = "
-    SELECT js.nama AS nama_jenis, SUM(ss.berat) AS total_berat
+    SELECT js.nama AS nama_jenis, SUM(ss.berat) AS total_berat, SUM(ss.berat * js.harga) AS total_pendapatan
     FROM setoran_sampah ss
     JOIN jenis_sampah js ON ss.id_jenis = js.id
+    $whereClause
     GROUP BY ss.id_jenis
 ";
 
 $result = mysqli_query($conn, $query);
 
-// Cek error SQL (jika ada)
-if (!$result) {
-    die("Query error: " . mysqli_error($conn));
-}
-
-// Siapkan array data untuk JSON
 $data = [];
 while ($row = mysqli_fetch_assoc($result)) {
     $data[] = [
         'jenis' => $row['nama_jenis'],
-        'berat' => (float)$row['total_berat']
+        'berat' => (float)$row['total_berat'],
+        'pendapatan' => (float)$row['total_pendapatan']
     ];
 }
 
-// Kembalikan sebagai JSON
 header('Content-Type: application/json');
 echo json_encode($data);
-?>
